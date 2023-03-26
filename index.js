63c10a6f5c89aad4fb0ed2aadb87bf11db3ed9e9
Suryakantdsa <dassuryakantadas@gmail.com>
2023-03-26 20:37:16 +0530

    js file

--- index.js
+++ index.js
@@ -1,95 +1,67 @@
-const hour = document.getElementById("hour")
-const min = document.getElementById("min")
-const sec = document.getElementById("sec")
-const milisec = document.getElementById("ms")
+let [hour, min, sec, milisec] = [0, 0, 0, 0]
+let timer = null;
+const display = document.querySelector(".display")
+const castDisplay = document.querySelector(".cast-display")
 
-const start = document.getElementById("start")
-// const start=document.querySelector(".start")
-
-const pause = document.getElementById("pause")
-const reset = document.getElementById("reset")
-const cast = document.getElementById("cast")
-
-function inMili(milC){
-    let k=setInterval(() => {
-        if (milC < 999) {
-            milC++;
-            milisec.innerHTML = milC
-        }
-        else{
-            // milisec.innerHTML="000"
-            clearInterval(k)
-        }
-    }, 1);
-}
-function inSec(sC){
-
-
-    let l=setInterval(() => {
-        if (sC<60) {
-            sC++;
-            sec.innerHTML = sC
-        }
-        else{
-            // sec.innerHTML="00"
-            clearInterval(l)
-        }
-    }, 1000);
-
-}
-function inMin(mC){
-   
-    let m=setInterval(() => {
-        if (mC<=60) {
-            mC++;
-            min.innerHTML = mC
-        }
-        else{
-            // min.innerHTML="00"
-            clearInterval(m)
-        }
-    }, 60000);
-}
-
-function inHour(hC){
-    
-    let n=setInterval(() => {
-        if (hC<=60) {
-            hC++;
-            hour.innerHTML = hC
-        }
-        else{
-            // hour.innerHTML="00"
-            clearInterval(n)
-        }
-    }, 360000);
-}
-start.addEventListener("click", function () {
-    let milC=0
-    let sC=0
-    let mC=0
-    let hC=0
-
-
-  inMili(milC)
-  inSec(sC)
-  inMin(mC)
-  inHour(hC)
-  
+document.querySelector(".start").addEventListener("click", () => {
+    if (timer !== null) {
+        clearInterval(timer)
+    }
+    console.log("start click")
+    timer = setInterval(displayTime, 10)
 })
 
-
-
-pause.addEventListener("click", function () {
-    console.log("pasued")
-    clearInterval(k)
-    clearInterval(l)
-    clearInterval(m)
-    clearInterval(n)
+document.querySelector(".reset").addEventListener("click", () => {
+    clearInterval(timer)
+    hour = 0;
+    min = 0;
+    sec = 0;
+    milisec = 0;
+    display.innerHTML = `00:00:00:000`
 })
-reset.addEventListener("click", function () {
+document.querySelector(".pause").addEventListener("click", () => {
+    clearInterval(timer)
 
 })
-cast.addEventListener("click", function () {
 
+const displayTime = () => {
+    milisec += 10;
+    if (milisec === 1000) {
+        milisec = 0
+        sec++
+        if (sec === 60) {
+            sec = 0
+            min++
+            if (min === 60) {
+                min = 0;
+                hour++
+            }
+        }
+    }
+
+    let h = hour < 10 ? "0" + hour : hour;
+    let m = min < 10 ? "0" + min : min
+    let s = sec < 10 ? "0" + sec : sec
+    // let ms=milisec<10?"00"+milisec:milisec
+    let ms = milisec < 10 ? "00" + milisec : (milisec < 100 ? "0" + milisec : milisec);
+
+
+    display.innerHTML = `${h}:${m}:${s}:${ms}`
+}
+let i = 1
+
+document.querySelector(".cast").addEventListener("click", () => {
+    const cast = document.createElement("p");
+
+
+    let h = hour < 10 ? "0" + hour : hour;
+    let m = min < 10 ? "0" + min : min
+    let s = sec < 10 ? "0" + sec : sec
+    // let ms=milisec<10?"00"+milisec:milisec
+    let ms = milisec < 10 ? "00" + milisec : (milisec < 100 ? "0" + milisec : milisec);
+
+
+    cast.innerText = `cast${i}: ${h}:${m}:${s}:${ms}  `
+    castDisplay.appendChild(cast)
+    i++
 })
